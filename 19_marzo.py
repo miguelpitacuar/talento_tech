ef86962a6617440b47e4044169f6fc22c25fb672
miguel pitacuar <miguelpitacuar83@gmail.com>
2024-03-19 21:43:05 -0500

    19 de marzo

---
+++ 19_marzo.py
@@ -0,0 +1,22 @@
+# edad = 16
+# tiene_licencia =False
+
+# if tiene_licencia==False:
+#     print("no tiene licencia ...no puede conducir ")
+    
+# if edad<18:
+#     print("no tiene  edad  para conducir ")
+
+# if edad>=18 and tiene_licencia==True:
+#     print("puedes conducir  tienes licencia y la mayoria de edad ")
+habla_ingles = False
+sabe_python = False
+
+if habla_ingles==True and sabe_python==True:
+    print("cumpler con los requisistos  de  ablar ingles y saber python")
+if habla_ingles==False and sabe_python==False:
+    print("no cumple con los requisitos  deve de saber python e ingles ")
+if habla_ingles==False:
+    print("deve de saber ingles  ")   
+if sabe_python==False:
+    print("deve de saber pyton") 
